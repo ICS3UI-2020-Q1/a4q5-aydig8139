2502373fbc6daf0ed3cd3786de673db69d5d70c6
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 15:41:39 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Hailstone sequence
+ * @author Rose
  */
 public class Main {
 
@@ -9,7 +11,36 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create new scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Ask user for a number
+    System.out.println("Please enter a positive integer to see the hailstone sequence.");
+    int usersNum = input.nextInt();
+    System.out.println("Hailstone sequence is the following:");
+
+    //create variable to determine if the number is even
+    
+    
+    
+
+
+    //create while loop
+    while(usersNum >= 1){
+      
+      int even = usersNum % 2;
+      
+      if (even == 0){
+        
+        System.out.println(usersNum);
+        usersNum = usersNum / 2;
+      }else if (even > 0){
+        System.out.println(usersNum);
+         usersNum = usersNum*3 + 1;
+      }
+     
+      
+    }
     
   }
 }
