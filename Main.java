35ab6151a1783f07f5678f97c7369174b152c18c
aydig8139 <71148103+aydig8139@users.noreply.github.com>
2020-10-05 11:06:43 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,31 +16,33 @@ public class Main {
 
     //Ask user for a number
     System.out.println("Please enter a positive integer to see the hailstone sequence.");
+
+    //declare variable to store user input
     int usersNum = input.nextInt();
+
+    
     System.out.println("Hailstone sequence is the following:");
 
-    //create variable to determine if the number is even
-    
-    
-    
 
-
-    //create while loop
-    while(usersNum >= 1){
+    //create while loop to list the hailstone sequence until the number before 1 
+    while(usersNum > 1){
       
+      //declare variable to determine if a number is even
       int even = usersNum % 2;
       
+      //create if statement to test if the number is even or not and print out the result
       if (even == 0){
-        
         System.out.println(usersNum);
         usersNum = usersNum / 2;
       }else if (even > 0){
         System.out.println(usersNum);
-         usersNum = usersNum*3 + 1;
+        usersNum = usersNum*3 + 1;
       }
+      }
+      //print out 1 to end the hailstone sequence
+      System.out.println("1");
      
       
     }
     
   }
-}
